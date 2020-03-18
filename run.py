d553ccc368d0d0a7044321260b4eecc9a4d32e08
Louise-LuLin <lulin199209@gmail.com>
2020-03-18 13:49:58 -0400

    Remove ignored files

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import os
 import sys
+import time
 sys.path.insert(1, './src')
 
 from tagger import CrfModel
 from data_loader import BuildDataLoader
 from environment import LabelEnv
 from agent import ParamRNN, AgentParamRNN
+from agent_trellisCNN import TrellisCNN, AgentTrellisCNN
+from agent_trellisRNN import TrellisRNN, AgentTrellisRNN
 
 from gensim.models import KeyedVectors
 import numpy as np
@@ -33,6 +36,7 @@ parser = argparse.ArgumentParser(description='Tune sequence active learning')
 parser.add_argument('-b', '--budget', type=int, help='budget size', default=75)                
 parser.add_argument('-f', '--feature', choices=['all', 'node', 'edge'], help='choose features', default='all') # test/decode TBD
 parser.add_argument('-g', '--greedy', help='top m test instances', default='rand')
+parser.add_argument('-m', '--model', help='model of encoding state', default='ParamRNN')
 parser.add_argument('-r', '--reward', help='shifted reward strategy', default='valid2V')
 parser.add_argument('-s', '--source', help='dataset', default='sod')                
 parser.add_argument('-u', '--upLoop', type=int, help='increase loop by N', default=10)
@@ -50,6 +54,7 @@ DATA_PATH = "./datasets/" + SOURCE
 NUM_FLAG = True if args.replaceX == 1 else False
 EMBED_FLAG = True if args.word2vec == 1 else False
 FEAT = args.feature
+MODEL = args.model
 
 dataloader = BuildDataLoader(SOURCE, DATA_PATH, NUM_FLAG, EMBED_FLAG)
 crf = CrfModel(dataloader, FEAT)
@@ -61,49 +66,71 @@ TRAIN_N = 600
 REWEIGHT = args.reward
 GEEDY = args.greedy
 BUDGET = args.budget
+RNN_HIDDEN = 128
+N_FILTER = 16
+FILTER_SIZE = 4
+N_STRIDE = 2
 max_len = dataloader.get_max_len()
 embedding_size = dataloader.get_embed_size()
 parameter_shape = crf.get_para_shape()
+trellis_shape = crf.get_trellis_shape()
 print ("max_len is: {}".format(max_len))
-print ("crf para size: {}".format(parameter_shape))
+print ("crf para shape: {}".format(parameter_shape))
+print ("crf trellis shape: {}".format(trellis_shape))
 
 # ======================================== active learning =====================================================
+# use trellis CNN to encode state
 qvalue_list = []
 action_mark_list = []
 prob_list = []
 for seed in samples:
     env = LabelEnv(dataloader, crf, seed, VALID_N, TEST_N, TRAIN_N, REWEIGHT, BUDGET)
-    agent = AgentParamRNN(GEEDY, max_len, embedding_size, parameter_shape)
+    if MODEL.startswith('trellisCNN'):
+        agent = AgentTrellisCNN(MODEL, GEEDY, max_len, embedding_size, trellis_shape, RNN_HIDDEN, N_FILTER, FILTER_SIZE, N_STRIDE)
+    elif MODEL.startswith('ParamRNN'):
+        agent = AgentParamRNN(GEEDY, max_len, embedding_size, parameter_shape, RNN_HIDDEN, N_FILTER, FILTER_SIZE, N_STRIDE)
+    else:
+        agent = AgentTrellisRNN(MODEL, GEEDY, max_len, embedding_size, trellis_shape, RNN_HIDDEN, N_FILTER, FILTER_SIZE, N_STRIDE)
+
     
-    print (">>>> Start play")
-    step = 0
+    print ("=== Start play ===")
+    step = 20
     while env.cost < BUDGET:
         env.resume()
+        # observ = [self.seq_embeddings, self.seq_confidences, self.seq_trellis, self.tagger_para, self.queried, self.scope]
+        
+        time1 = time.time()
         observation = env.get_state()
-        observ = [observation[0], observation[1], observation[3], observation[4], observation[5]] 
-        greedy_flg, action, q_value = agent.get_action(observ)
+        time2 = time.time()
+        greedy_flg, action, q_value = agent.get_action(observation)
+        time3 = time.time()
         reward, observation2, terminal = env.feedback(action)
+        time4 = time.time()
         (acc_test, acc_valid) = env.eval_tagger()
+        time5 = time.time()
         print ("cost {}: queried {} with greedy {}:{}, acc=({}, {})".format(env.cost, action, greedy_flg, 
                                                                             q_value.item(), acc_test, acc_valid))
+        print ("time: get_sate={}, get_action={}, feedback={}, eval_tagger={}".format(time2-time1, time3-time2, time4-time3, time5-time4))
+        
         qvalue_list.append(q_value.item())
         action_mark_list.append(greedy_flg)
         prob_list.append(observation[1][action])
         
         if env.cost % 10 == 0:
             step += 10
-        if env.cost < 3:
+        
+        if env.cost < 6:
             continue
-        for n in range(20 + step):
+         
+        for n in range(step):
             env.reboot()
             while env.terminal == False:
                 observation = env.get_state()
-                observ = [observation[0], observation[1], observation[3], observation[4], observation[5]] 
-                greedy_flg, action, q_value = agent.get_action(observ)
+                greedy_flg, action, q_value = agent.get_action(observation)
                 reward, observation2, terminal = env.feedback(action)
-                observ2 = [observation2[0], observation2[1], observation2[3], observation2[4], observation2[5]]
-                agent.update(observ, action, reward, observ2, terminal)
-
+                agent.update(observation, action, reward, observation2, terminal)
+        time6 = time.time()
+        print ("loop time: loop={}, time={}".format(step, time6-time5))
 # ======================================== store result =====================================================
 cost_list = sorted(env.acc_trace.keys())
 acc_list = [env.acc_trace[i][0] for i in cost_list]
@@ -111,7 +138,7 @@ acc_valid_list = [env.acc_trace[i][1] for i in cost_list]
 
 num = "num" if NUM_FLAG else ""
 emb = "embed" if EMBED_FLAG else ""
-filename = "./results_run/" + SOURCE + num + emb + "_" + str(BUDGET) + "bgt_" + GEEDY + "_" + REWEIGHT + "_" + FEAT
+filename = "./results_run/" + SOURCE + num + emb + "_" + MODEL + "_" + str(BUDGET) + "bgt_" + GEEDY + "_" + REWEIGHT + "_" + FEAT
 
 with open(filename + ".bin", "wb") as result:
     pickle.dump((cost_list, acc_list, acc_valid_list), result)
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-read -p "Enter source, budget, greedy, replaceX, word2vec, cuda: " s b g x w e
+read -p "Enter source, budget, model, greedy, replaceX, word2vec, cuda: " s b m g x w e
 echo
-reward=("valid2T" "valid2V" "test2T" "kmers")
+reward=("valid2V")
 for r in ${reward[@]}
 do
-    python run.py -s $s -b $b -r $r -g $g -x $x -w $w -e $e;
+    python run_trellis.py -s $s -b $b -m $m -r $r -g $g -x $x -w $w -e $e;
 done
